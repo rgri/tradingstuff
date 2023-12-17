be93a58b8fd527fb2b20ae51362e93345bbe3586
robbi <robert13grimley@gmail.com>
2023-12-16 23:22:51 -0600

    Stuff

--- trading.py
+++ trading.py
@@ -1,5 +1,6 @@
 import random
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import numpy as np
 import scipy
 
@@ -24,9 +25,42 @@ def geoBrownian(init=100, steps=100, mu=0, sigma=0.0025):
 
 
 steps = 1000
-y_points = geoBrownian(steps=steps)
 x_points = range(1, steps + 1)
 
-print(y_points)
-plt.plot(x_points, y_points)
-plt.show()
+trials = 100
+
+
+def get_cmap(n, name="hsv"):
+    """Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
+    RGB color; the keyword argument name must be a standard mpl colormap name."""
+    return plt.cm.get_cmap(name, n)
+
+
+colors = get_cmap(trials)
+
+# for i in range(trials):
+#     plt.plot(x_points, geoBrownian(steps=steps), color=colors(i + 1))
+
+# plt.show()
+
+
+def extractSubsequences(
+    series,
+    windowSize=10,
+    threshold=1,
+):
+    goodSubsequences = []
+    for i in range(0, len(series) - windowSize + 1):
+        window = series[i : windowSize + i]
+        try:
+            if series[windowSize + i] > threshold:
+                goodSubsequences[i] = window
+        except:
+            break
+    return goodSubsequences
+
+
+i = 0
+while i < len(extractSubsequences(geoBrownian(steps=steps))):
+    break
+    # pad the subsequences here
